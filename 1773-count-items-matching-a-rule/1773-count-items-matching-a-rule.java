db84c4702670151994be03767d94b1c6ac0ce710
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-10-19 23:05:00 +0600

    Time: 6 ms (72.11%), Space: 57 MB (18.01%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,14 +1,22 @@
 class Solution {
     
     public int countMatches(List<List<String>> arr, String ruleKey, String ruleValue) {
-        HashMap<String,Integer> rules = new HashMap<>();
-        rules.put("type",0);
-        rules.put("color",1);
-        rules.put("name",2);
+        byte rule = 0;
+        switch(ruleKey){
+            case "type":
+                rule = 0;
+                break;
+            case "color":
+                rule = 1;
+                break;
+            case "name":
+                rule = 2;
+                break;
+        }
         int counter = 0;
         for(int i = 0; i < arr.size(); i++){
             List<String> item = arr.get(i);
-            if(item.get(rules.get(ruleKey)).equals(ruleValue)) counter++;
+            if(item.get(rule).equals(ruleValue)) counter++;
         }
         return counter;
     }
