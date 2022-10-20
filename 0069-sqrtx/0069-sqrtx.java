1955d2b9b692d18202b8723c8765cbcacec9c231
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-10-20 09:04:16 +0600

    Time: 2 ms (84.76%), Space: 42.4 MB (7.00%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+      if(x <= 1) return x;
+        int start = 1;
+        int end = x/2;
+        
+        while(start < end) {
+            int mid = (start + (end-start)/2) + 1;
+            
+            int div = x/mid;
+            if(div == mid) return mid;
+            if(div > mid) start = mid;
+            else end = mid-1;
+        }
+        
+        return start;
     }
 }
