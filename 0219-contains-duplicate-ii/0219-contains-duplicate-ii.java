6b5d5d7d4fd01cd3ce8641d93a2fdda66d81a21a
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-10-21 09:22:35 +0600

    Time: 66 ms (16.98%), Space: 105.1 MB (5.02%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -5,11 +5,9 @@ class Solution {
             if(!hMap.containsKey(nums[i])){
                 hMap.put(nums[i],i);
             }else{
-                if(Math.abs(hMap.get(nums[i]) - i) <= k) return true;
-                else {
-                    hMap.remove(nums[i]);
-                    hMap.put(nums[i],i);
-                }
+                if(i - hMap.get(nums[i]) <= k) return true;
+                hMap.remove(nums[i]);
+                hMap.put(nums[i],i);
             }
         }
         return false;
