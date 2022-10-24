c8ddb370bf4e519ae082ec6935367aec751256a5
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-10-24 17:34:22 +0600

    Time: 53 ms (8.46%), Space: 59.3 MB (7.57%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -5,19 +5,13 @@ class Solution {
         
         for(int i = 0; i < nums.length; i++) {
             if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);   
+                map.put(nums[i],map.get(nums[i])+1);  
+                if(map.get(nums[i]) == nums.length/2) return nums[i];   
             }
             else{
                 map.put(nums[i],1);   
             }
         }
-        
-        for(int key : map.keySet()) {
-            if(map.get(key) == nums.length/2){
-               return key;   
-            }
-        }
-        
         return 0;
     }
 }
