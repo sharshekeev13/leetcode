71a31dbb5853b48b6168caf924bc01313cc6213e
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-10-29 01:36:32 +0600

    Time: 39 ms (5.95%), Space: 49.1 MB (6.79%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char n = board[i][j];
+
+                if (n != '.') {
+                    if (set.contains(n + "r" + i) || set.contains(n + "c" + j)
+                        || set.contains(n + "b" + i / 3 + j / 3)) {
+                        return false;
+                    }
+
+                    set.add(n + "r" + i);
+                    set.add(n + "c" + j);
+                    set.add(n + "b" + i / 3 + j / 3);
+                }
+            }
+        }
+
+        return true;
+    }
+}
