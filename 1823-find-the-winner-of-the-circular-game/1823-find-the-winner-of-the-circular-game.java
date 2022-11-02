7891d813a6b6bc534f7ade12a478c42d2924ae49
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-11-02 20:39:53 +0600

    Time: 0 ms (100.00%), Space: 41.7 MB (41.44%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int solve(int n, int k) {
+        if (n == 1)
+            return 0; 
+        return (solve(n - 1, k) + k) % n;
+    }
+    public int findTheWinner(int n, int k) {
+         return solve(n, k) + 1;
+    }
+}
