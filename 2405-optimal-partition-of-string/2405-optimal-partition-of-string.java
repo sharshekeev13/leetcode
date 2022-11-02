b9f1e3961e2704a8b60465366220b54eddb605c4
Dastan Sharshekeev <100109594+sharshekeev13@users.noreply.github.com>
2022-11-02 20:56:10 +0600

    Time: 67 ms (60.55%), Space: 71.5 MB (18.19%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,10 +1,9 @@
 class Solution {
     public int partitionString(String s) {
         int counter = 1;
-        String[] arr = s.split("");
         StringBuilder sb = new StringBuilder();
-        for(String c : arr){
-            if(sb.indexOf(c) == -1) sb.append(c);
+        for(char c : s.toCharArray()){
+            if(sb.indexOf(String.valueOf(c)) == -1) sb.append(c);
             else{
                 counter++;
                 sb.setLength(0);
